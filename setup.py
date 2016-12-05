0b1e6421b4d5cfc740e8a82cb86a9ea525d5e6af
Jonas Schneider <mail@jonasschneider.com>
2016-12-05 07:15:49 +0100

    Include runtimes.yml in pypi package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='universe',
-      version='0.20.1',
+      version='0.20.2',
       description="Universe: a software platform for measuring and training an AI's general intelligence across the world's supply of games, websites and other applications.",
       url='https://github.com/openai/universe',
       author='OpenAI',
@@ -24,4 +24,4 @@ setup(name='universe',
       extras_require={
           'atari': 'gym[atari]',
       }
-)
+      )
