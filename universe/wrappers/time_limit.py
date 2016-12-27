8024c390ff9f46261147819bf1181344f9e7f779
Jie Tang <jietang@openai.com>
2016-12-27 12:37:32 -0800

    Make variables private, adjust semantics to match gym

--- universe/wrappers/time_limit.py
+++ universe/wrappers/time_limit.py
@@ -11,11 +11,11 @@ DEFAULT_MAX_EPISODE_SECONDS = 20 * 60.  # Default to 20 minutes if there is no e
 class TimeLimit(vectorized.Wrapper):
     def _configure(self, **kwargs):
         super(TimeLimit, self)._configure(**kwargs)
-        self.max_episode_seconds = self.env.spec.tags.get('wrapper_config.TimeLimit.max_episode_seconds', None)
-        self.max_episode_steps = self.env.spec.tags.get('wrapper_config.TimeLimit.max_episode_steps', None)
+        self._max_episode_seconds = self.env.spec.tags.get('wrapper_config.TimeLimit.max_episode_seconds', None)
+        self._max_episode_steps = self.env.spec.tags.get('wrapper_config.TimeLimit.max_episode_steps', None)
 
-        if self.max_episode_seconds is None and self.max_episode_steps is None:
-            self.max_episode_seconds = DEFAULT_MAX_EPISODE_SECONDS
+        if self._max_episode_seconds is None and self._max_episode_steps is None:
+            self._max_episode_seconds = DEFAULT_MAX_EPISODE_SECONDS
 
         self._elapsed_steps = 0
         self._episode_started_at = None
@@ -26,11 +26,11 @@ class TimeLimit(vectorized.Wrapper):
 
     def _past_limit(self):
         """Return true if we are past our limit"""
-        if self.max_episode_steps is not None and self.max_episode_steps < self._elapsed_steps:
+        if self._max_episode_steps is not None and self._max_episode_steps <= self._elapsed_steps:
             logger.debug("Env has passed the step limit defined by TimeLimit.")
             return True
 
-        if self.max_episode_seconds is not None and self.max_episode_seconds < self._elapsed_seconds:
+        if self._max_episode_seconds is not None and self._max_episode_seconds <= self._elapsed_seconds:
             logger.debug("Env has passed the seconds limit defined by TimeLimit.")
             return True
 
